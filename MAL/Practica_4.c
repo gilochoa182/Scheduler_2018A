63de2158ace91eebd37d334bf6ffa8e59792b1f9
Gilberto Ochoa <uidp3590@ZUL1353G.cw01.contiwan.com>
2018-03-22 17:37:47 -0600

    Se crearon 2 nuevos archivos

---
+++ MAL/Practica_4.c
@@ -0,0 +1,8 @@
+/*
+ * Practica_4.c
+ *
+ *  Created on: 22/03/2018
+ *      Author: uidp3590
+ */
+
+
---
+++ MAL/Practica_4.h
@@ -0,0 +1,13 @@
+/*
+ * Practica_4.h
+ *
+ *  Created on: 22/03/2018
+ *      Author: uidp3590
+ */
+
+#ifndef PRACTICA_4_H_
+#define PRACTICA_4_H_
+
+
+
+#endif /* PRACTICA_4_H_ */
